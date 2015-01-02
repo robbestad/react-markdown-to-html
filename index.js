70078845b5c91e6b481d33e900e0a23e065c534f
Sven Anders Robbestad <robbestad@gmail.com>
2015-01-02 08:19:43 +0100

    changed input to src from file

--- index.js
+++ index.js
@@ -8,20 +8,16 @@ var marked = require('marked');
 var _ = require('lodash');
 
 module.exports = React.createClass({displayName: "Markdown2HTML",
-
   getInitialState: function(){
     return {
-      md: name
+      md: ''
     };
   },
   componentDidMount: function() {
-    $.get(this.props.file, _.bind(function(data) {
+    $.get(this.props.src, _.bind(function(data) {
       this.setState({md: marked(data)});
     }, this));
   },
-
-  // Render a title, the output portion, and the input portion.
-  // This is a very simple demo and akin to the one on the React homepage
   render: function(){
     return (
         React.createElement("div", {dangerouslySetInnerHTML: {__html: (this.state.md)}})
