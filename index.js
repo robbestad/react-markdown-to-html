340df1868b58119505ca1608c26c5c4eee9ffccc
Sven Anders Robbestad <robbestad@gmail.com>
2015-02-27 23:21:43 +0100

    updated render

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-/** @jsx React.DOM */
 "use strict";
 
-if("undefined" == typeof React)
-  var React = require('react');
+if ("undefined" == typeof React)
+    var React = require('react');
 
 var marked = require('marked');
 var _ = require('lodash');
 var $ = require('jquery');
 
-module.exports = React.createClass({displayName: "Markdown2HTML",
-  getInitialState: function(){
-    return {
-      md: ''
-    };
-  },
-  componentDidMount: function() {
-    $.get(this.props.src, _.bind(function(data) {
-      this.setState({md: marked(data)});
-    }, this));
-  },
-  render: function(){
-    return (
-        React.createElement("div", {dangerouslySetInnerHTML: {__html: (this.state.md)}})
-    );
-  }
+module.exports = React.createClass({
+    displayName: "Markdown2HTML",
+    getInitialState: function () {
+        return {
+            md: ''
+        };
+    },
+    componentDidMount: function () {
+        $.get(this.props.src, _.bind(function(data) {
+            this.setState({md: marked(data)});
+        }, this));
+    },
+    render: function () {
+        return <div ref="md" dangerouslySetInnerHTML={{__html: this.state.md}} />;
+    }
 });
